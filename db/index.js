0b0f12878f6ebf3d6983862875943452ff143683
Pablo Yocupicio <Pabloey@yahoo.com>
2021-12-21 17:08:21 -0700

    heroku deployment

--- db/index.js
+++ db/index.js
@@ -1,16 +1,38 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/characterDatabase';
+// let MONGODB_URI = 'mongodb://127.0.0.1:27017/characterDatabase';
+
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(() => {
+//     console.log('Successfully connected to MongoDB.');
+//   })
+//   .catch((e) => {
+//     console.error('Connection error', e.message);
+//   });
+
+// const db = mongoose.connection;
+
+// module.exports = db;
+
+const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/characterDatabase'
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(dbUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: true
+  })
   .then(() => {
-    console.log('Successfully connected to MongoDB.');
+    console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
-    console.error('Connection error', e.message);
-  });
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
+const db = mongoose.connection
 
-const db = mongoose.connection;
-
-module.exports = db;
+module.exports = db
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path')
 
 // require() imports and middleware here ^ ///////
 
@@ -23,6 +24,13 @@ app.get('/', (req, res) => {
   res.send('Hello character API')
 })
 const CONNECTION_URL = 'mongodb+srv://Pabloey:QkBB6hNKpLH62AAa@cluster0.ogp5o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
