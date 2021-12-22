a0f1883c2910203fa7ab8f22039b98e1b5a4fac3
Pablo Yocupicio <Pabloey@yahoo.com>
2021-12-21 19:17:15 -0700

    heroku push 2

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/api', routes);
 app.use(logger('dev'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/public')))
+  app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/public/index.html`))
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
   })
 }
 
